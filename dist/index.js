f398dd5445de2070dcb8cdaf9ddd8308b6448425
Noel da Costa <noeldacosta@gmail.com>
2026-07-17 20:34:03 +0100

    chore: build and bump version to 0.0.6

--- dist/index.js
+++ dist/index.js
@@ -21299,7 +21299,7 @@ class WelcomeAppShell extends SvelteComponent {
     flush();
   }
 }
-const version = "0.0.5";
+const version = "0.0.6";
 class WelcomeApplication extends SvelteApp {
   /**
    * Default Application options
